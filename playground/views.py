f6b90e44b953aef39309ed74722af62190386ac3
Shicai He <94800998+shicaih@users.noreply.github.com>
2023-01-11 01:56:05 -0500

    first commit

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.db.models import Q, F
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # query_set = Product.objects.filter(description__isnull=True)
+    # query_set = Customer.objects.filter(email__icontains='.com')
+    # query_set = Collection.objects.filter(featured_product__isnull = True)
+    # query_set = Product.objects.filter(inventory__lt=10)
+    # query_set = Order.objects.filter(customer__id=1)
+    # query_set = OrderItem.objects.filter(product__collection__id=3)
+    # query_set = Product.objects.filter(
+    #     Q(inventory__lt=10) | Q(unit_price__lt =20))
+    # query_set = Product.objects.filter(inventory=F('unit_price'))
+    query_set = Product.objects.order_by('title').reverse()
+    return render(request, 'hello.html', {'name': 'Shicai', 'products':list(query_set)})
